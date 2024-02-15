0091c4ffbb5565ff78065564aab9a496ed0dc7a4
K1S4L4 <chitogeyi@gmail.com>
2024-02-15 11:55:30 -0600

    i just realzied i ahavent done any commits till now... sorry!

---
+++ users.js
@@ -0,0 +1,9 @@
+let num1=4;
+let num2=12;
+document.getElementById("numone-El").textContent = num1
+document.getElementById("numtwo-El").textContent = num2
+let sumEl=document.getElementById("sum-el")
+function add(){
+let result=num1+num2
+    console.log(add)
+}
